a65d70626b21863c18a042d6c2dd282a61392d80
MohammedRayan04 <110118951+MohammedRayan04@users.noreply.github.com>
2024-07-18 07:52:00 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import styles from "./Hero.module.css";
 import heroDark from "@/assets/images/heroDark.png"; // Ensure this is the correct path
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -17,10 +18,14 @@ const Hero = () => {
         <h1 className={styles.welcomeText}>WELCOME TO</h1>
         <h2 className={styles.companyName}>SHIVA CATERERS</h2>
         <p className={styles.tagline}>Taste is the sign of life</p>
+        <Link href="/menu" legacyBehavior>
+          <a className={styles.heroButton}>Browse Menu</a>
+        </Link>
       </div>
     </div>
   );
 };
 
 export default Hero;
+
 
