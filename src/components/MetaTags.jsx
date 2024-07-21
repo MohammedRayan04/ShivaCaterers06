ee7f8deb484aa858cd714fb6c769a467f701949c
MohammedRayan04 <110118951+MohammedRayan04@users.noreply.github.com>
2024-07-21 23:55:31 +0530

    Update MetaTags.jsx

--- src/components/MetaTags.jsx
+++ src/components/MetaTags.jsx
@@ -1,25 +1,20 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-const MetaTags = ({ title, description, imageUrl, url }) => {
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      
-      {/* Open Graph Meta Tags */}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={imageUrl} />
-      <meta property="og:url" content={url} />
-      <meta property="og:type" content="website" />
-      
-      {/* Twitter Card Meta Tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imageUrl} />
-    </Head>
-  );
-};
+const MetaTags = ({ title, description, imageUrl, url }) => (
+  <Head>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={imageUrl} />
+    <meta property="og:url" content={url} />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content={imageUrl} />
+  </Head>
+);
 
 export default MetaTags;
+
